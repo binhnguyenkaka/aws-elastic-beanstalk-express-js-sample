7f10ad33294bb603177525d34e5fcd4283e4bb04
binhnguyenkaka <31397600+binhnguyenkaka@users.noreply.github.com>
2024-07-02 16:13:14 +0700

    5th change

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ app.get('/', (req, res) => res.send('Check Point Infinity AI Copilot - New'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
