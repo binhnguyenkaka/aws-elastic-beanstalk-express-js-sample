f9807326cfcfcb6471c09eb5c76ebf0e1bed519b
binhnguyenkaka <31397600+binhnguyenkaka@users.noreply.github.com>
2024-07-04 11:53:37 +0700

    6th change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Check Point Infinity AI Copilot - New'));
+app.get('/', (req, res) => res.send('Binh Nguyen'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
